7848e3804e020b91e8509b18b55af7a0a42a2ec4
Vipul Trimbake <130178066+VipulTrimbake610@users.noreply.github.com>
2023-06-21 04:35:50 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+	const strong = document.getElementsByTagName("strong");
 function highlight() {
     //Write your code here
 
+	for(let i=0;i<strong.length;i++){
+		strong[i].style.color = "green";
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+    	for(let i=0;i<strong.length;i++){
+		strong[i].style.color = "black";
+	}
 }
